23b46fc38069889dae36bf674a7a399e4984d778
Michael McLaren <m.mclaren42@gmail.com>
2021-02-17 13:21:38 -0500

    Sketch Models section

---
+++ _common.R
@@ -0,0 +1,19 @@
+set.seed(42)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = FALSE,
+  echo = FALSE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+options(crayon.enabled = FALSE)
+
+# library(tidyverse)
+library(here)
