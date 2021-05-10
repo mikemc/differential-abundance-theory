ed61736e4e242e72bf695be1ce99f345b615e18c
Michael McLaren <m.mclaren42@gmail.com>
2021-05-10 07:40:02 -0400

    Import article from Google Docs
    Also includes editing to fix formatting as well as some minor text
    improvements.

--- _common.R
+++ _common.R
@@ -5,15 +5,20 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   cache = FALSE,
   echo = FALSE,
-  out.width = "70%",
-  fig.align = 'center',
-  fig.width = 6,
-  fig.asp = 0.618,
+  # out.width = "70%",
+  # fig.align = 'center',
+  # fig.width = 6,
+  # fig.asp = 0.618,
   fig.show = "hold"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
 options(crayon.enabled = FALSE)
 
-# library(tidyverse)
+library(tidyverse)
 library(here)
+library(cowplot)
+library(patchwork)
+theme_set(theme_cowplot(12))
+
+close_elts <- function(x) x / sum(x)
