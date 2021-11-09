6b7dc9045ed0ce59ade913478bb1cf3ddc8fdc4a
Michael McLaren <m.mclaren42@gmail.com>
2021-11-09 16:11:39 -0500

    Fix figure rendering in pdf output
    The calls to `knitr::include_graphics()` were failing in pdf output
    because these calls have to be included in a top level R statement. This
    commit fixes this issue; it also removes code redundancy, switches to
    relative paths (the recommended method for bookdown), and puts the pdf
    images in a new folder contained within the project directoty.

--- _common.R
+++ _common.R
@@ -22,3 +22,20 @@ library(patchwork)
 theme_set(theme_cowplot(12))
 
 close_elts <- function(x) x / sum(x)
+
+# Create a pdf from an svg with inkscape
+# svg_path should be relative to project root
+svg_to_pdf <- function(svg_path) {
+  fig_path <- fs::path(
+    'figures', 'export-pdf', 
+    fs::path_ext_set(svg_path, 'pdf')
+  )
+  args <- c(
+    "--export-area-page", 
+    "--export-type=pdf", 
+    str_c("--export-filename=", fig_path),
+    svg_path
+  )
+  system2("inkscape", args = args)
+  fig_path
+}
